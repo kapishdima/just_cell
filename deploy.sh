c61e2bdf3f09ffcc38574ada919531bd182c589d
DimaDevelopment <dima.development@fakel.com.ua>
2023-04-22 21:02:45 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@ yarn build:staging
 # navigate into the build output directory
 cd dist
 
-git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:kapishdima/just_cell.git master:gh-pages
