7c6bdd8c6f9e18cc8272e8120145ad4c5e81002a
DimaDevelopment <dima.development@fakel.com.ua>
2023-09-19 22:06:09 +0300

    create points pages

---
+++ src/modules/points/pages/CreateFiscalPointPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <app-layout>
+    <template #appTitle>Додати нову точку/відділення</template>
+    <template #appContent>
+      <div class="department-content">
+        <upload-fiscal-key @upload="onSubmit" />
+      </div>
+    </template>
+  </app-layout>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import AppLayout from "@/components/layout/AppLayout/AppLayout.vue";
+import UploadFiscalKey from "../ui/UploadFiscalKey.vue";
+
+export default defineComponent({
+  components: {
+    AppLayout,
+    UploadFiscalKey,
+  },
+  methods: {
+    onSubmit(values: any) {
+      console.log(values);
+    },
+  },
+});
+</script>
+<style lang=""></style>
